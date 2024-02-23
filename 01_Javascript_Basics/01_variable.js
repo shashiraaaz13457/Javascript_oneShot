310b309a2b8e0b7e856270914bc8cda7b45cf68b
Shashi Raj <shashiraj13457@gmail.com>
2024-02-23 20:09:57 +0000

    variables in Javascript

---
+++ 01_Javascript_Basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "shashiraj13457@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" //not good
+let accountState;
+
+// accountId = 2 //not allowed
+
+accountEmail = "hitesh@gmail.com"
+accountPassword = "212142"
+accountCity = "Bengaluru"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
