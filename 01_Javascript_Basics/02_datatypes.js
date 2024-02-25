5170fbb755c4a87dfe432d6897be2d56bbe0a34b
Shashi Raj <shashiraj13457@gmail.com>
2024-02-25 06:58:30 +0000

    datatypes

---
+++ 01_Javascript_Basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code a newer version
+
+//alert(3+3) // we are using nodejs , not browser
+
+console.log(3
+    +
+    3) // code readability is low
+
+console.log("Shashi");
+
+let name = "Shashi"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//Primitive datatypes
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standalone value
+//undefined => no value assigned till now
+// symbol => uniqueness(mainly used while studying React)
+
+// object
+
+console.log(typeof "Shashi"); //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
