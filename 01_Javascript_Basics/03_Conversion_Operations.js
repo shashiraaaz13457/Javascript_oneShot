575f06a9966286f46cffacaec51de851eced6174
Shashi Raj <shashiraj13457@gmail.com>
2024-02-26 03:17:24 +0000

    conversion of datatypes

---
+++ 01_Javascript_Basics/03_Conversion_Operations.js
@@ -0,0 +1,35 @@
+// let score =  33
+// let score = "33"
+// let score = "33abc"
+// let score = null 
+// let score = undefined
+// let score = true
+let score = "hitesh"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)  //Conversion from String to Number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//Conversion in Number
+//"33" => 33
+//"33abc" => NaN(NOt a Number)
+//true => 1 / false => 0
+
+
+// let isLoggedIn = 1   //true  0 => false
+// let isLoggedIn = ""   // false
+let isLoggedIn = "hitesh"    //true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
