1a08e80dee420c6a8cd195da8999b09b4d864a34
Shashi Raj <shashiraj13457@gmail.com>
2024-02-26 03:51:56 +0000

    Conversion and Operation in javascript

--- 01_Javascript_Basics/03_Conversion_Operations.js
+++ 01_Javascript_Basics/03_Conversion_Operations.js
@@ -30,6 +30,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33
 
 let StringNumber = String(someNumber);
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
 
+
+
+//****************Operations*****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);  //2^3 = 8
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Shashi"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  //12
+// console.log(1 + "2");  //12
+// console.log("1" + 2 + 2);   //122
+// console.log(1 + 2 + "2");   //32
+
+// If one or both operands are strings, the + operator is used for string concatenation. If both operands are numbers, it performs addition. JavaScript performs type coercion to handle these situations.
+console.log(+true); //bad Practice
+console.log(+"");   //bad 
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2  //bad practice
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
