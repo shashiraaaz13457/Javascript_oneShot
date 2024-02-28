591b0397de8b09545201fcad6ca1421b458d6123
Shashi Raj <shashiraj13457@gmail.com>
2024-02-28 07:49:08 +0000

    Comparison Operators

---
+++ 01_Javascript_Basics/04_comparisons.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2>=1); // true(something unusual) Because this operand checks for the left operand is greater or not only.
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2<=1); 
+
+//For different datatypes
+// console.log("2" > 1);  // Here javascript implicitly convert the string into Number.
+// console.log("02" > 1);
+
+//Null Case(Comparison convert null into a number or in NaN, treating it as 0) not equality sign
+// console.log(null > 0);   //false 
+// console.log(null >= 0);  //true
+// console.log(null == 0);  //false (not treating null as 0)
+// console.log(null < 0);  //false
+
+//undefined Case
+// console.log(undefined == 0); //false
+// console.log(undefined > 0);  //false
+// console.log(undefined < 0);  //false
+// console.log(undefined < 1);  //false
+// console.log(undefined == 1);  //false
+// console.log(undefined > 0);  //false
+
+// (===) Strict Check (Check Value as well as datatype)
+console.log("2" === 2);
