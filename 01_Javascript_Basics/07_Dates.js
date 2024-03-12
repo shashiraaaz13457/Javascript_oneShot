8d3737aef07ad02d4af630d005e8fe78bbf2dfd7
Shashi Raj <shashiraj13457@gmail.com>
2024-03-12 09:44:46 +0000

    Dates

--- 01_Javascript_Basics/07_Dates.js
+++ 01_Javascript_Basics/07_Dates.js
@@ -12,5 +12,28 @@ let myDate = new Date()
 
 // let myCreateDate = new Date(2023,0,23) //You have inserted your date
 // console.log(myCreateDate.toDateString());
-let myCreateDate = new Date(2023,0,23,5,3) //You can use differnt Parameters
-console.log(myCreateDate.toLocaleString()); //Includes time also
+// let myCreateDate = new Date(2023,0,23,5,3) //You can use differnt Parameters
+// console.log(myCreateDate.toLocaleString()); //Includes time also
+// let myCreateDate = new Date("2023-01-14")
+let myCreateDate = new Date("01-14-2023");
+// console.log(myCreateDate.toLocaleDateString());
+
+let myTimestamp = Date.now();
+// console.log(myTimestamp);
+// console.log(myCreateDate.getTime());
+
+//converting to second
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);  // As it starts from 0.
+console.log(newDate.getDay()); //starting from monday
+
+//Very Imp in Dates
+//You can specify more configuration whatever you wants in the Date.
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
+
+
