18eabdf8f57396ee1f66c08e221379fca7918a42
Shashi Raj <shashiraj13457@gmail.com>
2024-07-18 01:01:12 +0000

    For-in loops

--- 05_Iterations/02_Some_Other_loops.js
+++ 05_Iterations/02_Some_Other_loops.js
@@ -13,11 +13,11 @@ for (const greet of greetings) {
 
 //Maps
 
-const map = new Map()
-map.set('IN',"India")
-map.set('IN',"India")// Unique value in maps.(Not print it second time)
-map.set('US',"United States of America")
-map.set('Fr',"France")
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('IN',"India")// Unique value in maps.(Not print it second time)
+// map.set('US',"United States of America")
+// map.set('Fr',"France")
 
 // console.log(map);
 
@@ -25,12 +25,12 @@ map.set('Fr',"France")
     // console.log(i);
 // }
 
-for (const [key,value] of map) { //Destructing of array
-    console.log(key, ':-' , value);
-}
-for (const [key] of map) {
-    console.log(key);
-}
+// for (const [key,value] of map) { //Destructing of array
+//     console.log(key, ':-' , value);
+// }
+// for (const [key] of map) {
+//     console.log(key);
+// }
 
 // const myObject = { //Objects are not iteratable.
 //     'Game1' : 'NFS',
@@ -39,5 +39,51 @@ for (const [key] of map) {
 
 // for (const [key,value] of myObject) {
 //     console.log(key, ":-" , value);
+// }
+
+// For Object - We use For-in loop(For-of loop is not used)
+const myObject = {
+    js : 'javascript',
+    cpp : 'C++',
+    swift : 'swift by Apple',
+    py : 'Python'
+}
+
+// for (const key in myObject) {
+//     console.log(key);
+// }
+
+// for (const key in myObject) {
+//     console.log(myObject[key]);
+// }
+for (const key in myObject) {
+    console.log(`${key} is shortcut for ${myObject[key]}`);
+}
+
+//For-in loop for array.
+
+const programming = ["js","rb","py"]
+
+//This gives the the key i.e. index (For-of loop directly gives the value but not in case of for-in loop)
+// for (const key in programming) {
+//     console.log(key);
+// }
+
+//This will gives the value at that particular index.
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+//Now for-in loops for Maps
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('IN',"India")
+// map.set('US',"United States of America")
+// map.set('Fr',"France")
+
+// //Map is not iteratable(NO result)
+// for (const key in map) {
+//     console.log(key);
 // }
 
